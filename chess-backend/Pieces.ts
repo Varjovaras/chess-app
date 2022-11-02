7e40b8b897d204bcaee21378ec450ccc8e22bdb4
Kristjan Rajaniemi <Kristjan.rajaniemi@live.fi>
2022-11-02 12:49:41 +0200

    Updated chessboard creation

---
+++ chess-backend/Pieces.ts
@@ -0,0 +1,11 @@
+class Pieces {}
+
+export enum Piece {
+	pawn = 'PAWN',
+	knight = 'KNIGHT',
+	bishop = 'BISHOP',
+	rook = 'ROOK',
+	queen = 'QUEEN',
+	king = 'KING',
+	empty = 'EMPTY',
+}
--- chess-backend/chess.ts
+++ chess-backend/chess.ts
@@ -1,37 +1,81 @@
-import { Piece, Square, SquareColor } from './types';
+enum SquareColor {
+	black = 'BLACK',
+	white = 'WHITE',
+}
+
+type Square = {
+	rank: number;
+	file: string;
+	square: string;
+	color: SquareColor;
+	pieceName: PieceName;
+	id: number;
+};
+
+enum file {
+	a = 1,
+	b,
+	c,
+	d,
+	e,
+	f,
+	g,
+	h,
+}
+
+enum PieceName {
+	pawn = 'PAWN',
+	knight = 'KNIGHT',
+	bishop = 'BISHOP',
+	rook = 'ROOK',
+	queen = 'QUEEN',
+	king = 'KING',
+	empty = 'EMPTY',
+}
 
 class Chess {
-	board: Square[] = new Array(64);
+	_board: Square[] = new Array(64);
+
+	static DEFAULT_POSITION =
+		'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+	static idiot = '';
+
 	constructor() {
 		let files: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-		let currentFile = 0;
 		let firstSquare = SquareColor.black;
 		let secondSquare = SquareColor.white;
+		let rank = 1;
 
-		for (let i = 0, rank = 1; i < this.board.length; i++, rank++) {
-			if (rank === 9) {
+		for (
+			let i = 0, currentFile = 0;
+			i < this._board.length;
+			i++, currentFile++
+		) {
+			if (currentFile === 8) {
+				currentFile = 0;
+				rank++;
 				let temp: SquareColor = firstSquare;
 				firstSquare = secondSquare;
 				secondSquare = temp;
-				rank = 1;
-				currentFile++;
 			}
 
 			if (i % 2 === 0) {
-				this.board[i] = {
+				this._board[i] = {
 					file: files[currentFile],
 					rank: rank,
 					color: firstSquare,
 					square: `${files[currentFile]}${rank}`,
-					piece: Piece.empty,
+					pieceName: PieceName.empty,
+					id: i,
 				};
 			} else {
-				this.board[i] = {
+				this._board[i] = {
 					file: files[currentFile],
 					rank: rank,
 					color: secondSquare,
 					square: `${files[currentFile]}${rank}`,
-					piece: Piece.empty,
+					pieceName: PieceName.empty,
+					id: i,
 				};
 			}
 		}
@@ -40,16 +84,49 @@ class Chess {
 	printBoard() {
 		let rows = ['', '', '', '', '', '', '', ''];
 
-		for (const i of this.board) {
+		for (const i of this._board) {
+			console.log(i);
 			rows[i.rank - 1] += i.file + i.rank + ' ';
 		}
-
 		rows.reverse();
+		console.log(this._board);
 		return rows;
 	}
 
-	startingPosition() {}
+	startingPosition() {
+		const fen = Chess.DEFAULT_POSITION;
+		let tokens = fen.split(/\s+/);
+		let pieces = tokens[0].split('/');
+		return pieces;
+	}
+
+	movePiece() {
+		/**
+		 *
+		 */
+	}
+
+	putPiece(square: string, piece: PieceName) {
+		let sq = this._board.find((s: Square) => s.square === square);
+		if (sq) {
+			sq.pieceName = piece;
+			console.log(`${piece} put on ${square}`);
+		} else {
+			console.log('No square found');
+		}
+	}
+
+	fen(fen: string) {
+		let tokens = fen.split(/\s+/);
+		let pieces = tokens[0].split('/');
+		return pieces;
+	}
 }
 
+class Pieces {}
+
 const chess = new Chess();
+// console.log(chess._board);
 console.log(chess.printBoard());
+// console.log(chess.startingPosition());
+// chess.putPiece('a1', PieceName.queen);

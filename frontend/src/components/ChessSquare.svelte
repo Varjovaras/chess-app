12276b65e9eb8243be26cb6bdf24ada75ed3a54d
Kristjan Rajaniemi <Kristjan.rajaniemi@live.fi>
2023-01-23 08:50:32 +0200

    Simple css

--- frontend/src/components/ChessSquare.svelte
+++ frontend/src/components/ChessSquare.svelte
@@ -5,12 +5,14 @@
 	// console.log(sq);
 </script>
 
-{#if sq.getPiece !== null}
-	<div class="">
-		{sq.getPiece?.getFirstLetter()}
-	</div>
-{:else}
-	<div>
-		{sq.getSquareName}
-	</div>
-{/if}
+<div class="text-blue-200">
+	{#if sq.getPiece !== null}
+		<p class="my-1">
+			{sq.getPiece?.getFirstLetter()}
+		</p>
+	{:else}
+		<p class="my-1">
+			{sq.getSquareName}
+		</p>
+	{/if}
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,39 +1,26 @@
 <script lang="ts">
+	import Form from '../components/Form.svelte';
 	import ChessSquare from '../components/ChessSquare.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 	export let board = data.board.getBoardWhite();
 	console.log(board);
-	let startSq = '';
-	let endSq = '';
-	let lastMove: { startSq: string; endSq: string } = { startSq, endSq };
 </script>
 
-<div class="">
-	<h1 class="">chessing</h1>
-	<div class="grid grid-cols-8">
+<div class="flex flex-wrap ">
+	<h1 class="w-full font-bold text-2xl">chessing</h1>
+	<div class="grid grid-cols-8 w-80">
 		{#each board as row}
 			{#each row as sq}
 				<ChessSquare {sq} />
 			{/each}
 		{/each}
 	</div>
-	<!-- <form class="box-border flex flex-col w-80">
-		<div>move</div>
-
-		<input
-			bind:value={startSq}
-			placeholder="enter start square"
-			class="flex justify-between top-1"
-		/>
-		<input bind:value={endSq} placeholder="enter end square" />
-		<button type="submit">Submit</button>
-	</form> -->
-	<p>Last move: {lastMove.startSq} {lastMove.endSq}</p>
+	<Form />
 </div>
 
 <style lang="postcss">
 	:global(html) {
-		background-color: #8c92ac;
+		background-color: theme(colors.gray.500);
 	}
 </style>

5344f7711132455ba6049db0237666f552dc9d97
Kristjan Rajaniemi <Kristjan.rajaniemi@live.fi>
2023-01-26 13:05:31 +0200

    Button for resetting the game

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
-	import Chess from '../../../backend/chess/chess';
 	import ChessSquare from '../components/ChessSquare.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 	export let board = data.board.getBoardWhite();
-	let startSq = "";
-	let endSq = "";
-	let lastMove = {startSq: startSq, endSq: endSq};
+	let startSq = '';
+	let endSq = '';
+	let lastMove = { startSq: startSq, endSq: endSq };
+	let moves: string[] = [];
 	const handleChess = () => {
-		console.log("123")
-		data.chess.move(startSq,endSq);
+		data.chess.move(startSq, endSq);
 		board = data.chess.getBoard.getBoardWhite();
-		 startSq = "";
-		 endSq = "";	
-		 }
+		lastMove.startSq = startSq;
+		lastMove.endSq = endSq;
+		moves = data.chess.getMovesAsString();
+		startSq = '';
+		endSq = '';
+		console.log(moves);
+	};
+
+	const resetChess = () => {
+		data.chess.startingPosition();
+		board = data.chess.getBoard.getBoardWhite();
+		startSq = '';
+		endSq = '';
+		console.log('Chess resetted');
+	};
 </script>
 
 <div class="grid h-full place-items-center">
@@ -27,22 +38,26 @@
 		<div>
 			<form class="box-border flex flex-col w-80" on:submit|preventDefault={handleChess}>
 				<p class="my-1">move</p>
-		
+
 				<input
 					bind:value={startSq}
 					placeholder="enter start square"
 					class="flex justify-between my-1"
 				/>
 				<input bind:value={endSq} placeholder="enter end square" class="my-2 w-full" />
-				<button type="submit">Submit</button>
+				<button type="submit" class="my-1 border-4 border-slate-800">Submit</button>
+				<button type="button" on:click={resetChess} class="my-1 border-4 border-slate-800 w-full"
+					>Reset game</button
+				>
+				<p class="my-1">Moves of the game: {lastMove.startSq} {lastMove.endSq}</p>
+				<p class="my-1">Last move: {lastMove.startSq} {lastMove.endSq}</p>
 			</form>
-			<p class="">Last move: {lastMove.startSq} {lastMove.endSq}</p>
 		</div>
-		</div>
+	</div>
 </div>
 
 <style lang="postcss">
 	:global(html) {
 		background-color: gray;
 	}
-  </style>
+</style>
